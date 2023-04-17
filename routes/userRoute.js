25d5f8988205182e041b6867826d592c19dd30d1
vishal1145 <greaserevive@gmail.com>
2023-04-18 00:30:46 +0530

    change folder structure

---
+++ routes/userRoute.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUser, getSingleUser, updateUserRole, deleteUser } = require('../controllers/userController')
+const { isAuthenticatedUser } = require('../middleware/auth')
+
+
+
+router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
+router.route("/logout").get(logout)
+router.route("/password/forgot").post(forgotPassword)
+router.route("/password/reset/:token").put(resetPassword)
+router.route("/me").get(isAuthenticatedUser ,getUserDetails)
+router.route("/password/update").put(isAuthenticatedUser ,updatePassword)
+router.route("/me/update").put(isAuthenticatedUser ,updateProfile)
+router.route("/admin/users").get(isAuthenticatedUser ,getAllUser)
+router.route("/admin/user/:id").get(isAuthenticatedUser ,getSingleUser).put(isAuthenticatedUser ,updateUserRole).delete(isAuthenticatedUser,deleteUser)
+module.exports =router
